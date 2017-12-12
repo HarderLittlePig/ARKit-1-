25b8d94bfe45531470ff99d0b37a06c52c23b8cf
HarderLittlePig <1104396899@qq.com>
2017-12-12 18:05:20 +0800

    这里是注释---初始化提交

--- ARKit初认识/ViewController.swift
+++ ARKit初认识/ViewController.swift
@@ -17,39 +17,47 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
-        sceneView.delegate = self
+        // 存放所有 3D 几何体的容器
+        let scene = SCNScene()
+        //几何(想要绘制的 3D 立方体)
+        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        //渲染(可以渲染颜色或者图片)
+        let material = SCNMaterial()
+        //设置渲染的填充内容
+        material.diffuse.contents = UIImage(named:"header")
+        //渲染是一个数组,可以添加多种渲染
+        box.materials = [material]
+        //节点(将几何体包装为 node 以便添加到 scene)
+        let boxNode = SCNNode(geometry: box)
+        // 把 box 放在摄像头正前方
+        boxNode.position = SCNVector3(0,0,-0.5)
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        // rootNode 是一个特殊的 node，它是所有 node 的起始点
+        scene.rootNode.addChildNode(boxNode)
         
-        // Set the scene to the view
+        // 将 scene 赋给 view
         sceneView.scene = scene
+        //设置默认光照
+        //sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
+        // 创建运行世界跟踪配置(世界跟踪配置跟踪设备的方向和位置。它还可以通过设备的相机检测现实世界的表面)
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
+        
+        // 开启运行世界跟踪
         sceneView.session.run(configuration)
     }
+    
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        // Pause the view's session
+        // 停止跟踪视图内容的运动和处理图像
         sceneView.session.pause()
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
     }
 
     // MARK: - ARSCNViewDelegate
